b665769fa0363e6dc2d1acaa59ab7d811b787c2d
tomas perez <tomasperezpsn@outlook.com>
2024-11-17 13:40:19 -0300

    Vercel error

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      // Aquí puedes agregar opciones de configuración específicas de Rollup si las necesitas
+      input: './src/main.jsx', // Cambia esto si tu entrada es diferente
+      output: {
+        dir: 'dist', // Carpeta de salida para los archivos generados
+        format: 'es', // Formato de salida (puedes cambiarlo si necesitas otro tipo)
+      },
+    },
+  },
 })
